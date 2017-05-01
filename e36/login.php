61927c351f180bbb78053d949dca5eed22664dc4
LaurisLatiss <lauris.eduards@gmail.com>
2017-05-01 18:59:58 +0300

    Add files via upload
    Lapas dizains

---
+++ e36/login.php
@@ -0,0 +1,41 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1 shrink-to-fit=no">
+    <title>Login</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="carousel.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <link href="https://fonts.googleapis.com/css?family=Comfortaa" rel="stylesheet">
+ </head>
+ <body>
+
+ <div class="header">
+     <a href="index.html">Return</a>
+ </div>
+
+    <h1>Login</h1>
+    <span></span>
+
+    <form action="login.php" method="POST">
+        
+        <input type="text" placeholder="Username" name="username">
+        <input type="password" placeholder="Password" name="password">
+        <input type="submit">
+
+    </form>
+ 	
+
+
+
+    
+
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+ </body>
+</html>
